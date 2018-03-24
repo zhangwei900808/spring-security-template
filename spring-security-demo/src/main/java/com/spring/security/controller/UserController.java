1cc2e95719f6ce95428480f1acb055432fe643c8
张威 <zw900808@gmail.com>
2018-03-24 16:15:48 +0800

    完成查询功能

---
+++ spring-security-demo/src/main/java/com/spring/security/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.spring.security.controller;
+
+import com.spring.security.dto.User;
+import com.spring.security.dto.query.UserQueryCondition;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    @GetMapping
+    public List<User> query(UserQueryCondition condition,
+                            @PageableDefault(page = 2, size = 12, sort = "username,desc") Pageable pageable) {
+        System.out.println(ReflectionToStringBuilder.toString(condition, ToStringStyle.MULTI_LINE_STYLE));
+        System.out.println(ReflectionToStringBuilder.toString(pageable, ToStringStyle.MULTI_LINE_STYLE));
+
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        return users;
+    }
+}
